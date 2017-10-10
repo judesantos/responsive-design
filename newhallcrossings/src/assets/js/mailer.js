f5ae8b094d9d02ec5d897ad8215d235ab31478fa
judesantos <jude@yourtechy.com>
2017-10-09 22:03:08 -0700

    validation: cancel submit on failed

--- newhallcrossings/src/assets/js/mailer.js
+++ newhallcrossings/src/assets/js/mailer.js
@@ -8,13 +8,14 @@ function send_mail(type, url, formData, success_cb, error_cb) {
       dataType: 'text', // what type of data do we expect back from the server
       encode: true,
       success: function (response) {
-        if (typeof success_cb !== 'undefined') {
+        if (typeof success_cb !== 'undefined'&& typeof response !== 'undefined' ) {
           success_cb(response);
         }
       },
       error: function (error) {
-        if (typeof error_cb !== 'undefined') {
-          error_cb(error);
+        if (typeof error_cb !== 'undefined' && typeof error !== 'undefined') {
+          var status = error.status + ': ' + error.statusText
+          error_cb(status);
         }
       },
   })
